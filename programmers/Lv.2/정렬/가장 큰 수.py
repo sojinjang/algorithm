0782e9ee9e20ef2126e34fc5e09abf1be456ada4
sojinjang <jsojin6119@gmail.com>
2022-10-22 16:02:48 +0900

    :technologist: solve 가장 큰 수

---
+++ programmers/Lv.2/정렬/가장 큰 수.py
@@ -0,0 +1,48 @@
+def special_mergeSort(alist):
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        left_list = alist[:mid]
+        right_list = alist[mid:]
+
+        special_mergeSort(left_list)
+        special_mergeSort(right_list)
+
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(left_list) and j < len(right_list):
+            left = int(str(left_list[i]) + str(right_list[j]))
+            right = int(str(right_list[j]) + str(left_list[i]))
+
+            if left < right:
+                alist[k] = left_list[i]
+                i += 1
+            else:
+                alist[k] = right_list[j]
+                j += 1
+            k += 1
+
+        while i < len(left_list):
+            alist[k] = left_list[i]
+            i += 1
+            k += 1
+
+        while j < len(right_list):
+            alist[k] = right_list[j]
+            j += 1
+            k += 1
+
+    return alist
+
+
+def solution(numbers):
+    answer_list = special_mergeSort(sorted(numbers))
+    answer_list.reverse()
+    answer = ""
+    for i in answer_list:
+        answer += str(i)
+    if answer[0] == "0":
+        answer = "0"
+
+    return answer
