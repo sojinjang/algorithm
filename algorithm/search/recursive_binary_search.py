5cf4c8cc86ca1e0ccce1f71a2a0829f0beafc1e2
sojinjang <jsojin6119@gmail.com>
2022-12-25 10:04:16 +0900

    :recycle: refactor: 파라미터명 변경

--- algorithm/search/recursive_binary_search.py
+++ algorithm/search/recursive_binary_search.py
@@ -1,12 +1,12 @@
-def recursive_bs(input_arr, x, low, high):
+def recursive_bs(input_arr, target, low, high):
     if high >= low:
         mid = low + (high - low) // 2
-        if input_arr[mid] == x:
+        if input_arr[mid] == target:
             return mid
-        elif input_arr[mid] > x:
-            return recursive_bs(input_arr, x, low, mid - 1)
-        elif input_arr[mid] < x:
-            return recursive_bs(input_arr, x, mid + 1, high)
+        elif input_arr[mid] > target:
+            return recursive_bs(input_arr, target, low, mid - 1)
+        elif input_arr[mid] < target:
+            return recursive_bs(input_arr, target, mid + 1, high)
     else:
         return -1
 
