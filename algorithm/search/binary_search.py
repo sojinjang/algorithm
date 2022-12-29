24674cc0a80c8788bb8e71c2a12ef9c05628c576
sojinjang <jsojin6119@gmail.com>
2022-12-29 16:13:44 +0900

    :technologist: implement binary search (recursive)

--- algorithm/search/binary_search.py
+++ algorithm/search/binary_search.py
@@ -10,6 +10,17 @@ def iterative_bs(input_arr, target, low, high):
 
     return -1
 
+def recursive_bs(array, x, low, high):
+    if high >= low:
+        mid = low + (high - low) // 2
+        if array[mid] == x:
+            return mid
+        elif array[mid] > x:
+            return recursive_bs(array, x, low, mid - 1)
+        elif array[mid] < x:
+            return recursive_bs(array, x, mid + 1, high)
+    else:
+        return -1
 
 array = [3, 4, 5, 6, 7, 8, 9]
 x = 4
