5675ef3da65dc96d62522baeb65ddf9e433bcc85
sojinjang <jsojin6119@gmail.com>
2023-01-01 11:15:23 +0900

    :fire: remove: recursive 코드 삭제

--- algorithm/search/binary_search.py
+++ algorithm/search/binary_search.py
@@ -10,18 +10,6 @@ def iterative_bs(input_arr, target, low, high):
 
     return -1
 
-def recursive_bs(input_arr, x, low, high):
-    if high >= low:
-        mid = low + (high - low) // 2
-        if input_arr[mid] == x:
-            return mid
-        elif input_arr[mid] > x:
-            return recursive_bs(input_arr, x, low, mid - 1)
-        elif input_arr[mid] < x:
-            return recursive_bs(input_arr, x, mid + 1, high)
-    else:
-        return -1
-
 
 array = [3, 4, 5, 6, 7, 8, 9]
 x = 4
