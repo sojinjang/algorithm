a42f2a566f8ab889faa6fa5e1cc214b58a1eb0ff
sojinjang <jsojin6119@gmail.com>
2023-02-13 11:05:23 +0900

    :technologist: 동적계획법: solve 등굣길

---
+++ programmers/Lv.3/동적계획법(Dynamic Programming)/등굣길.py
@@ -0,0 +1,10 @@
+def solution(m, n, puddles):
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    dp[1][1] = 1
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if [j, i] in puddles: continue
+            dp[i][j] += (dp[i - 1][j] + dp[i][j - 1]) % 1000000007
+
+    return dp[n][m]
